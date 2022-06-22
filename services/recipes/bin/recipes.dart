18b414fe7fd7b304149080e4011b0a560e272320
Youri Lieverdink <89806453+YouriLieverdink@users.noreply.github.com>
2022-06-22 19:47:44 +0200

    feat: add gracefull shutdown for services

--- services/recipes/bin/recipes.dart
+++ services/recipes/bin/recipes.dart
@@ -1,4 +1,18 @@
+import 'dart:io';
+
 import 'package:recipes/recipes.dart';
 import 'package:shelf_plus/shelf_plus.dart';
 
-void main() => shelfRun(() => init(prefix), defaultBindAddress: '0.0.0.0');
+void main() async {
+  //
+  final handler = () => init(prefix);
+  final context = await shelfRun(handler, defaultBindAddress: '0.0.0.0');
+
+  // Ensures a gracefull shutdown.
+  ProcessSignal.sigint.watch().listen((_) async {
+    await context.close();
+    await db.close();
+
+    exit(0);
+  });
+}
